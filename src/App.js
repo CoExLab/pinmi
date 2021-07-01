b666343ac83e315fe9a5ff937a883afc57d1a3ca
emoneil <emmaaoneil@gmail.com>
2021-06-30 21:39:58 -0700

    pin-mi video call components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import {ActiveStepProvider, PinsProvider} from './context/index'
 import Content from './components/layout/Content';
+import { Provider } from "react-redux";
+import { store } from "./components/Store";
 // import 'default-passive-events';
 /* "default-passive-events": "^2.0.0", */
 
 const App = () => {
     return (
-        <ActiveStepProvider>
-            <PinsProvider>
-                <main>
-                    <Content />
-                </main>
-            </PinsProvider>
-        </ActiveStepProvider>
+        <Provider store={store}>
+            <ActiveStepProvider>
+                <PinsProvider>
+                    <main>
+                        <Content />
+                    </main>
+                </PinsProvider>
+            </ActiveStepProvider>
+        </Provider>
     )
 }
 
--- src/components/layout/PracticeSession.jsx
+++ src/components/layout/PracticeSession.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-    
+import VideoChatComponent from "../VideoChatComponent.js";
+
 const PracticeSession = () => {
     return (  
         <div>
             <h1> Practice Session </h1>
+            <VideoChatComponent />
         </div>
     );
 }
