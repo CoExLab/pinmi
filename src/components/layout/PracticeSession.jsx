9cecbd1a2c15fb8ff790c70ecd6768fc1ef55d03
Yuanqin Fan <fanyuanqin96@gmail.com>
2021-07-09 01:16:09 -0400

    added refresher, self reflection section

--- src/components/layout/PracticeSession.jsx
+++ src/components/layout/PracticeSession.jsx
@@ -7,31 +7,35 @@ const PracticeSession = () => {
     const [apiKey, setApiKey] = useState("YOUR_API_KEY");
     const [sessionId, setSessionId] = useState("YOUR_SESSION_ID");
     const [token, setToken] = useState("YOUR_TOKEN");
+    const [readyMessage, setReadyMessage] = useState("video is not ready");
+
+    const fetchServerRes = (setApiKey, setSessionId, setToken, baseURL) => {
+        setReadyMessage("preparing video cal for you now...");
+        fetch(baseURL).then(function(res) {
+            return res.json()
+          }).then(function(res) {
+            console.log("got server info");
+            setApiKey(res.apiKey);
+            setSessionId(res.sessionId);
+            setToken(res.token);
+            setReadyMessage("video call is ready now");
+          }).catch((error) => {console.log(error)});
+    }
 
     return (  
         <div>
             <h1> Practice Session </h1>
             <button onClick = 
             {() => {fetchServerRes(setApiKey, setSessionId, setToken, baseURL)}}
-            >Click me for a new session token</button>
-            <h1>apiKeys: {apiKey}</h1>
+            >Click me for getting video call ready</button>
+            {/* <h1>apiKeys: {apiKey}</h1>
             <h1>sessionId: {sessionId}</h1>
-            <h1>token: {token}</h1>
+            <h1>token: {token}</h1> */}
+            <h3>{readyMessage} </h3>
             
             <VideoChatComponent apiKey = {apiKey} sessionId = {sessionId} token = {token}/>
         </div>
     );
-}
-
-const fetchServerRes = (setApiKey, setSessionId, setToken, baseURL) => {
-    fetch(baseURL).then(function(res) {
-        return res.json()
-      }).then(function(res) {
-        setApiKey(res.apiKey);
-        setSessionId(res.sessionId);
-        setToken(res.token);
-        //initializeSession();
-      }).catch((error) => {console.log(error)});
 }
 
 
