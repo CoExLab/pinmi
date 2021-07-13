56bb9d069155383ae176aba61ae6cc84fdfb7093
Yuanqin Fan <fanyuanqin96@gmail.com>
2021-07-13 10:22:55 -0400

    implemented transcript display

--- src/components/VideoChatComponent.js
+++ src/components/VideoChatComponent.js
@@ -93,6 +93,8 @@ function VideoChatComponent(props) {
   const { pins, setPins } = usePins();
   // get document ID
   const pinID = generatePushId();
+  // get trans ID
+  const transID = generatePushId();
   // hard-coded sessionID here
   const MiTrainingSessionID = "123";
 
@@ -117,6 +119,24 @@ function VideoChatComponent(props) {
     console.log("finished writing")      
     console.log(curTime);  
   }
+
+  const addTranscript = async () => {
+    await firebase.firestore().collection("Transcripts").doc("testSessionID").set({
+      text: results
+    })
+    .then( () => {
+        setPins([...pins, ]);
+    })
+    .then(() => {
+        console.log("Document successfully written!");    
+    })
+    .catch((error) => {
+        console.error("Error writing document: ", error);
+    });  
+    console.log("finished writing transcript")    
+  }
+
+
 
   const {
     error,
@@ -241,6 +261,8 @@ function VideoChatComponent(props) {
   const handleStopChat = () => {
     setIsInterviewStarted(false);
     stopSpeechToText();
+    //add trans to firebase
+    addTranscript();
     console.log(results);
   }
 
