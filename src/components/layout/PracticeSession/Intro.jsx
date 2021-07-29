6638a56e174af71835f869f12fa0a8f74fd70338
mragarwa <mragarwa@andrew.cmu.edu>
2021-07-29 13:47:50 -0400

    conditional rendering for practice session on the correct branch

---
+++ src/components/layout/PracticeSession/Intro.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Intro = () => {
+    return (  
+        <div>
+            <h1> Now, it’s time to step into the practice session and practice using open-ended questions. </h1>
+            <p>Click on the pin to create time marks of</p>
+            <ul>
+                <li>situations where you struggled to use MI</li>
+                <li>instances of effective MI use</li>
+            </ul>
+            <p>Your peer will also be pinning, and you will review and discuss all pins after the client session.</p>
+        </div>
+    );
+}
+
+
+export default Intro;
