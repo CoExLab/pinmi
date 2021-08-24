c89038afea4052c7512bf708ffc2491ef7ff4b55
ta211 <55717622+ta211@users.noreply.github.com>
2021-08-24 14:32:49 -0400

    Adds landing pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import {ActiveStepProvider, PinsProvider, SessionProvider, UserModeProvider} from './context/index'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+import {ActiveStepProvider, PinsProvider, SessionProvider, UserModeProvider} from './context/index';
+import Landing from './components/layout/Landing';
 import Content from './components/layout/Content';
 import { Provider } from "react-redux";
 import { store } from "./components/Store";
@@ -8,19 +14,24 @@ import { store } from "./components/Store";
 
 const App = () => {
     return (
-        <Provider store={store}>
-            <SessionProvider>
-                <ActiveStepProvider>
-                    <PinsProvider>
-                        <UserModeProvider>
-                            <main>
-                                <Content />
-                            </main>
-                        </UserModeProvider>
-                    </PinsProvider>
-                </ActiveStepProvider>
-            </SessionProvider>
-        </Provider>
+        <Router>
+            <Provider store={store}>
+                <SessionProvider>
+                    <ActiveStepProvider>
+                        <PinsProvider>
+                            <UserModeProvider>
+                                <main>
+                                    <Switch>
+                                        <Route exact path='/' component={Landing}/>
+                                        <Route exact path="/content" component={Content}/>
+                                    </Switch>
+                                </main>
+                            </UserModeProvider>
+                        </PinsProvider>
+                    </ActiveStepProvider>
+                </SessionProvider>
+            </Provider>
+        </Router>
     )
 }
 
