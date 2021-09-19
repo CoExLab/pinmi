93a191006a358d10663a5e191ac81a391a3a408d
ta211 <55717622+ta211@users.noreply.github.com>
2021-09-19 19:42:34 -0400

    ColorLibPaper component style update

--- src/components/layout/ColorLibComponents/ColorLibPaper.jsx
+++ src/components/layout/ColorLibComponents/ColorLibPaper.jsx
@@ -4,12 +4,20 @@ import Paper from '@material-ui/core/Paper';
 
 const ColorLibPaper = withStyles((theme) => ({
   root: {
+  },
+  elevation0: {
+    // For Self Reflection Page
     backgroundColor: theme.palette.teal.lighter,
+    borderRadius: '30px',
     boxShadow: 'none',
     padding: '50px 80px',
   },
-  rounded: {
-    borderRadius: '30px',
-  }
+  elevation9: {
+    // For Refresher Page final answer preview
+    borderRadius: '10px',
+    boxShadow: '0px 0px 9px rgba(51, 126, 146, 0.15)',
+    padding: '6px 14px',
+  },
 }))(Paper);
+
 export default ColorLibPaper;
