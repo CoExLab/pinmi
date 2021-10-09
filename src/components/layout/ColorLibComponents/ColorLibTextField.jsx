c2b74d925b09a29127128b4d2108fa198688ebc4
ta211 <55717622+ta211@users.noreply.github.com>
2021-10-09 17:49:06 -0400

    Fixes Textfield weird behavior; Updates Refresher to store user submitted answers in database

--- src/components/layout/ColorLibComponents/ColorLibTextField.jsx
+++ src/components/layout/ColorLibComponents/ColorLibTextField.jsx
@@ -1,46 +1,36 @@
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 import TextField from '@material-ui/core/TextField';
-
-// const ColorLibTextField = withStyles((theme) => ({
-//   root: {
-//     marginTop: '8px',
-//     '&.MuiInputLabel-root': {
-//       color: theme.palette.teal.main,
-//     },
-//   }
-// }))(TextField);
-
-const ColorLibTextField = (props) => {
-  const CustomizedTextField = withStyles((theme) => ({
-    root: {
-      '& .MuiInputLabel-outlined': {
-        color: theme.palette.gray.main,
-        opacity: '60%',
+const CustomizedTextField = withStyles((theme) => ({
+  root: {
+    '& .MuiInputLabel-outlined': {
+      color: theme.palette.gray.main,
+      opacity: '60%',
+    },
+    '& .MuiInputBase-input': {
+      color: theme.palette.gray.main,
+    },
+    '& .MuiOutlinedInput-root': {
+      borderRadius: '5px',
+      '& fieldset': {
+        borderColor: theme.palette.teal.light,
+        borderWidth: '1.5px',
       },
-      '& .MuiInputBase-input': {
-        color: theme.palette.gray.main,
+      '&:hover fieldset': {
+        borderColor: theme.palette.teal.main,
+        borderWidth: '1.5px',
       },
-      '& .MuiOutlinedInput-root': {
-        borderRadius: '5px',
-        '& fieldset': {
-          borderColor: theme.palette.teal.light,
-          borderWidth: '1.5px',
-        },
-        '&:hover fieldset': {
-          borderColor: theme.palette.teal.main,
-          borderWidth: '1.5px',
-        },
-        '&.Mui-focused fieldset': {
-          borderColor: theme.palette.teal.main,
-          borderWidth: '1.5px',
-        }
+      '&.Mui-focused fieldset': {
+        borderColor: theme.palette.teal.main,
+        borderWidth: '1.5px',
       }
     }
-  }))(TextField);
+  }
+}))(TextField);
 
+const ColorLibTextField = (props) => {
   return (
-    <CustomizedTextField 
+    <CustomizedTextField
       {...props}
       label={props.label ?? "Type a response..."}
     />
