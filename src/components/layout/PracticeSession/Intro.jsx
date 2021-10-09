cdc3dcfa97155207a13ae1631c3ed27b0b029501
ta211 <55717622+ta211@users.noreply.github.com>
2021-10-09 17:49:34 -0400

    code formatting

--- src/components/layout/PracticeSession/Intro.jsx
+++ src/components/layout/PracticeSession/Intro.jsx
@@ -11,48 +11,48 @@ import Box from '@material-ui/core/Box';
 
 
 const useStyles = makeStyles({
-    table: {
-      width: 600,
-    },
-  });
+  table: {
+    width: 600,
+  },
+});
 
 
 function createData(name, role, fat, carbs, protein) {
-    return { name, role, fat, carbs, protein };
-  }
-  
-  const rows = [
-    createData('Your role', 'Therapist', 6.0, 24, 4.0),
-    createData('Your peers role', 'Client', 237, 9.0, 37, 4.3),
-    createData('Your goal', 'Build rapport and analyze the client’s situation', 262, 16.0, 24, 6.0),
-  ];
-  
+  return { name, role, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Your role', 'Therapist', 6.0, 24, 4.0),
+  createData('Your peers role', 'Client', 237, 9.0, 37, 4.3),
+  createData('Your goal', 'Build rapport and analyze the client’s situation', 262, 16.0, 24, 6.0),
+];
+
 const Intro = () => {
-    const classes = useStyles();
-    return (  
-      <div>
-        <Box align="center" m = {2}>
-             <h1>Now, it’s time to step into the practice session and practice using open-ended questions.</h1>
-            <TableContainer>
-      <Table className={classes.table} aria-label="simple table">
-        
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-              <Box fontWeight="fontWeightBold">{row.name}</Box>
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {row.role}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>  
-    </Box>  
-        </div>
-    );
+  const classes = useStyles();
+  return (
+    <div>
+      <Box align="center" m={2}>
+        <h1>Now, it’s time to step into the practice session and practice using open-ended questions.</h1>
+        <TableContainer>
+          <Table className={classes.table} aria-label="simple table">
+
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    <Box fontWeight="fontWeightBold">{row.name}</Box>
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.role}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </div>
+  );
 }
 
 
