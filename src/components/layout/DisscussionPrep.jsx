0475ceffa0522524b1c0d4ca5027a98e3be31762
ta211 <55717622+ta211@users.noreply.github.com>
2021-10-25 15:36:42 -0400

    Solves Transcript section shrinking on empty content problem

--- src/components/layout/DisscussionPrep.jsx
+++ src/components/layout/DisscussionPrep.jsx
@@ -40,6 +40,8 @@ const useStyles = makeStyles((theme) => ({
         position: 'absolute',
         top: 0,
         bottom: 0,
+        left: 0,
+        right: 0,
         overflowY: 'scroll',
       }
     },
