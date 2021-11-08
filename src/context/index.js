f7d06f1d55db3812e900044fc0b42ad991a1f64e
ergitlin <60827429+ergitlin@users.noreply.github.com>
2021-11-08 15:42:54 -0500

    Revert "minor changes"
    This reverts commit 1ca8d9758d58222c482ad7447b1f9a5bea9e1888.

--- src/context/index.js
+++ src/context/index.js
@@ -15,28 +15,10 @@ export const ActiveStepProvider = ({children}) => {
 export const useActiveStepValue = () => useContext(ActiveStepContext);
 
 export const UserModeContext = createContext();
-
-// var userID = "";
-// const setUserID = (oldID, newID) => {
-//   console.log("in user id");
-//   userID.substr(0,0);
-//   userID.concat(newID);
-//   console.log(newID);
-//   console.log(userID);
-// }
-
-// var userMode = "";
-// const setUserMode = (oldMode, newMode) => {
-//   console.log("In user mode");
-//   userMode.substr(0, 0);
-//   userMode.concat(newMode);
-//   console.log(newMode);
-//   console.log(userID);
-// }
-
 export const UserModeProvider = ({children}) => {
-    const [userID, setUserID] = useState("");
     const [userMode, setUserMode] = useState("");
+    const [userID, setUserID] = useState("");
+
     return  (
         <UserModeContext.Provider value = {{userMode, setUserMode, userID, setUserID}}>
             {children}
@@ -45,8 +27,10 @@ export const UserModeProvider = ({children}) => {
 }
 export const useUserModeValue = () => useContext(UserModeContext);
 
+
 export const SessionContext = createContext();
 const newDoc = randomString(19);
+
 //temporarily use date as sessionID
 const date = new Date();
 const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
