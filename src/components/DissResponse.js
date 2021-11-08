df73a20ad4fee3eceed77e2461b50317ff3664b0
ergitlin <60827429+ergitlin@users.noreply.github.com>
2021-11-08 15:42:50 -0500

    Revert "Merge branch 'UserTesting' into tempUsabilityTesting"
    This reverts commit 25aa887960c8fe33911dc6f08ebea5a5a7745a25, reversing
    changes made to 73fee94c66a0463fcced44542f1a7d1da6aa6231.

--- src/components/DissResponse.js
+++ src/components/DissResponse.js
@@ -39,6 +39,7 @@ const DissResponse = ({ curPinIndex, setCurPinIndex, prevPinIndex, setPrevPinInd
     const classes = useStyles();
 
     //creating a refernce for TextField Components
+    const efficacyValueRef = useRef('')
     const goalValueRef = useRef('')
     const strengthValueRef = useRef('')
     const opportunityValueRef = useRef('')
@@ -63,15 +64,18 @@ const DissResponse = ({ curPinIndex, setCurPinIndex, prevPinIndex, setPrevPinInd
     const [curSkillInfo1, setCurSkillInfo1] = useState('');
     const [curSkillInfo2, setCurSkillInfo2] = useState('');
 
+    const [curEfficacyInfo, setCurEfficacyInfo] = useState(pins[curPinIndex].pinEfficacy);
     const [curGoalInfo, setCurGoalInfo] = useState(pins[curPinIndex].pinGoal);
     const [curStrengthInfo, setCurStrengthInfo] = useState(pins[curPinIndex].pinStrength);
     const [curOpporunityInfo, setCurOpportunityInfo] = useState(pins[curPinIndex].pinOpportunity);
 
     useEffect(() => {
         fetchCurTextVal();
+        pins[prevPinIndex].pinEfficacy = curEfficacyInfo;
         pins[prevPinIndex].pinGoal = curGoalInfo;
         pins[prevPinIndex].pinStrength = curStrengthInfo;
         pins[prevPinIndex].pinOpportunity = curOpporunityInfo;
+        setCurEfficacyInfo(pins[curPinIndex].pinEfficacy);
         setCurGoalInfo(pins[curPinIndex].pinGoal);
         setCurStrengthInfo(pins[curPinIndex].pinStrength);
         setCurOpportunityInfo(pins[curPinIndex].pinOpportunity);
@@ -244,6 +248,24 @@ const DissResponse = ({ curPinIndex, setCurPinIndex, prevPinIndex, setPrevPinInd
                         value={curSkillInfo2}
                     />
                 </form>
+
+                <Box textAlign="left">
+                    <Typography>
+                        Why was the pinned situation effective or ineffective?
+                    </Typography>
+                </Box>
+                <ColorLibTextField
+                    id="outlined-secondary"
+                    label="Personal Notes..."
+                    fullWidth
+                    variant="outlined"
+                    multiline
+                    rows={3}
+                    margin="normal"
+                    value={curEfficacyInfo}
+                    inputRef={efficacyValueRef}
+                    onChange={() => setCurEfficacyInfo(efficacyValueRef.current.value)}
+                />
 
                 <Box textAlign="left">
                     <Typography>
