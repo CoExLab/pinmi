e5e8c5dcd36dfc90715b9c80a1549491f6dfe226
mragarwa <mragarwa@andrew.cmu.edu>
2021-12-09 21:44:08 -0500

    removed loading

--- src/components/layout/PracticeSession/Loading.jsx
+++ src/components/layout/PracticeSession/Loading.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { useActiveStepValue, usePinsValue } from "../context";
+import { useActiveStepValue, usePinsValue } from "../../../context";
 import { useSelector } from "react-redux";
 
 
 
-export default const Loading = (props) => {
+export default function Loading(props) {
     //require isReady and finishLoading functions, and we call it by props.isReady
     //isReady: send a get request - ENTEREDROOM(vonage api sessionid)
     //once isReady returns true, run finishLoading
     const { curActiveStep: activeStep, setCurActiveStep: setActiveStep } = useActiveStepValue();
     const session = useSelector(state => state.session);
 
-    return (
-        boooooo
-    )
 }
