a36375d1257aacf4ef5df73768e58f9052b8711a
eshitakar <eshitakar3@gmail.com>
2021-12-14 02:45:20 -0500

    Change to DB pre-session doc creation to support existing users

--- src/other/setSessionsV1.py
+++ src/other/setSessionsV1.py
@@ -6,7 +6,7 @@ import csv
 import random
 
 # Use the application default credentials
-cred = credentials.Certificate("./pinmi-59c77-firebase-adminsdk-z692d-cb44b7b3d2.json")
+cred = credentials.Certificate("../../../pinmi-59c77-firebase-adminsdk-z692d-955bbf0b26.json")
 firebase_admin.initialize_app(cred)
 
 
@@ -22,19 +22,30 @@ csvwriter = csv.DictWriter(output_file, fieldnames)
 csvwriter.writeheader()
 
 for row in input_file:
+    #Generate first and last name, as well as username
     name = row["Name"].split()
     fname = name[0]
     lname = name[1]
     username = fname.lower() + lname.lower()
-    userid = uuid.uuid4().hex
+    #check if userid already exists (user is already existent)
+    userid = ""
+    doc = db.collection(u'users').document(username).get()
+    if(doc.exists):
+        userid = doc.to_dict().get('userID')
+        print("User: " + username + " already exists")
+    else:
+        userid = uuid.uuid4().hex
+        print("User: " + username + " is new")
+    #assign role
     trole = row["Role (Therapist/Client)"]
     role = ""
     if(trole == "Therapist"):
         role = "caller"
     else:
         role = "callee"
+    #create dictionary key-obj entry
     udict[username] = {"first": fname, "last": lname, "userID": userid, "username": username, "sessionid": "", "role": role}
-    #csvwriter.writerow(dict(row, username='%s' % username))
+    #printing for confirmation
     user = udict[username]
     print("first: " + user['first'] + " last: " + user['last'] + " userid: " + user['userID'] + " curSession: " + user['sessionid'])
 input_file = csv.DictReader(open("users.csv"))
@@ -45,11 +56,13 @@ for row in input_file:
     pname = row["Partner"].split()
     if(len(pname) > 1):
         username = name[0].lower() + name[1].lower()
+        #check if the username is bad
         if(len(pname) > 1): 
             pusername = pname[0].lower() + pname[1].lower()
         else:
             pusername = "na" + str(count)
             count += 1
+        #if the sessionid is empty, it hasn't been assigned for both the partner and the user
         if(udict[username]["sessionid"] == ""):
             sessionid = uuid.uuid4().hex
             prevuser = udict[username]
@@ -58,6 +71,7 @@ for row in input_file:
             pprevuser = udict[pusername]
             pprevuser["sessionid"] = sessionid
             udict[pusername] = pprevuser
+        #if it's been assigned, put in the roels
         else:
             caller = {}
             callee = {}
@@ -68,6 +82,7 @@ for row in input_file:
                 caller = udict[pusername]
                 callee = udict[username]
             print("caller id: " + caller["userID"] + " callee id: " + callee["userID"])
+            #create the session doc
             doc_ref = db.collection(u'sessions').document(udict[username]["sessionid"])
             doc_ref.set({
                 u'caller_id': caller['userID'],
@@ -88,5 +103,4 @@ for uN in udict:
         u'userID': user['userID'],
         u'curSession': user['sessionid']
     })
-    print("first: " + user['first'] + " last: " + user['last'] + " userid: " + user['userID'] + " curSession: " + user['sessionid'])
-
+    print(user['username'] + " added")
